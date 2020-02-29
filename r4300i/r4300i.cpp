7e0954f27a232d63ba56c07bd0d7112b1f52ddb5
nim-ka <raumaank1@gmail.com>
2020-02-29 13:51:09 -0500

    interp stuff

---
+++ r4300i/r4300i.cpp
@@ -0,0 +1,6 @@
+#include "r4300i.hpp"
+#include <cstdio>
+
+int main() {
+	R4300iState s;
+}
---
+++ r4300i/types.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define byte	unsigned char
+#define hword	short
+#define word	int
+#define dword	long int
+
